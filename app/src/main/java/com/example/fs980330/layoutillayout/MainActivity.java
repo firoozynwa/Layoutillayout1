06ce6c91380dc12ecc236a160f2d29623dedf0ff
Safdari <firoz.safdari@skola.gavle.se>
2016-09-22 11:11:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fs980330/layoutillayout/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.fs980330.layoutillayout;
+
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView htmlCVText = (TextView) findViewById(R.id.textView);
+        htmlCVText.setText(Html.fromHtml(getString(R.string.CV)));
+    }
+
+    private void nyMetod(){
+
+        int i = 1+1;
+        
+
+
+    }
+}
+
+
